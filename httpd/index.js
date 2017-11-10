841e9bc71134a23d3711a2da38256d2ebc6dafbd
LZJ861124 <bii861124@gmail.com>
2017-11-10 14:55:13 +0800

    index

--- httpd/index.js
+++ httpd/index.js
@@ -1,5 +1,23 @@
 'use strict';
+
 let http = require('http');
+
+const routingTable = {
+  '/': {
+    url: '../htdocs/index.html',
+    type: 'text/html'
+  },
+
+  '/assets/css/styles.css': {
+
+    url: '../htdocs/assets/css/styles.css',
+    type: 'text/css'
+  },
+  '/assets/png/SokobanClone_byVellidragon.png',
+  type: 'image/png'
+  },
+};
+
 /**
 * 利⽤ http.ServerResponse 物件回傳檔案內容
 *
@@ -12,6 +30,7 @@ let http = require('http');
 */
 let serve = (response, fname, datatype) => {
   let fs = require('fs');
+
   fs.readFile(fname, (err, data) => {
     if (err) {
       console.log(' 檔案讀取錯誤');
@@ -20,46 +39,44 @@ let serve = (response, fname, datatype) => {
       response.writeHead(200, {
         'Content-Type': datatype
       });
+
       response.write(data);
       response.end();
     }
   });
 };
+
+
 http.createServer((request, response) => {
   let fs = require('fs');
+
   let postData = '';
-  10
+
   // 利⽤ 'data' event 消耗掉 data chunk;
   // 'end' event 才會被 fired
   request.on('data', (chunk) => {
     postData += chunk;
+
     console.log(
       ' 接收的 POST data ⽚段 k: [' + chunk + '].'
     );
   });
+
   request.on('end', () => {
-    switch (request.url) {
-      case '/':
-      serve(response, '../htdocs/index.html', 'text/html');
-      break;
-      case '/assets/css/styles.css':
-      serve(response, '../htdocs/assets/css/styles.css', 'text/css');
-      break;
-      case '/assets/png/SokobanClone_byVellidragon.png':
-      serve(
-        response,
-        '../htdocs/assets/png/SokobanClone_byVellidragon.png',
-        'image/png'
-      );
-      break;
-      default:
+    if (request.url in routingTable) {
+      let obj = routingTable[request.url];
+
+      serve(response, obj.url, obj.type);
+    }
+    else {
       console.log(' 未定義的存取: ' + request.url);
+
       response.end();
-      break;
     }
   });
 }).listen(8088);
+
 // log message to Console
-11
 console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+
 // index.js
